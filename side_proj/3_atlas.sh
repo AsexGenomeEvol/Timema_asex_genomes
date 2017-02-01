b154f3008e1848da2f362ef73e29032d89fae5bb
Kamil S Jaroň <376090@mail.muni.cz>
2017-02-01 14:30:30 +0100

    [variant_analysis] structural variants will not have PacBIo reads

--- side_proj/3_atlas.sh
+++ side_proj/3_atlas.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
-# index fasta specified by 1st argument
+# species
+# insert size
+# ref name
 
 bsub <<< """
 #BSUB -L /bin/bash
-#BSUB -J index_fa
+#BSUB -J callBayes_$1
 #BSUB -q priority
 #BSUB -o atlas.out
 #BSUB -e index_fa.err
@@ -18,11 +20,7 @@ LOCAL_DIR=/scratch/local/monthly/kjaron/$1/$2
 export TMP_DIR=\$LOCAL_DIR/temp
 mkdir -p \$LOCAL_DIR/temp
 
-GenomeAnalysisTK \
-  -T HaplotypeCaller \
-  -R \$TROOT/data/$1/reference/$3 \
-  -I \$TROOT/data/$1/variant_calling/GATK/$2/map_pe_to_$1.bam \
-  --genotyping_mode DISCOVERY \
-  -stand_call_conf 30 \
-  -o raw_variants.vcf
+atlas task=callBayes \
+  bam=\$TROOT/data/$1/variant_calling/GATK/$2/map_pe_to_$1.bam \
+  1> callBayes.out 2> callBayes.err¬
 """
