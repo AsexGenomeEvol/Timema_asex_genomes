f129249ded0e9d86f91b5236f5ca4e2ce5755a78
Kamil S Jaroň <376090@mail.muni.cz>
2017-06-05 19:25:20 +0200

    [variant_analysis] next iteration of atlas attempts

--- side_proj/atlas_estTheta.sh
+++ side_proj/atlas_estTheta.sh
@@ -6,8 +6,14 @@
 # window_size
 
 SP=$1
-BAM="$3"_to_"$2".bam
+REF=$2
+IND=$3
 WINDOW=$4
+
+BAMBASE="$IND"_to_"$REF"
+BAM="$BAMBASE".bam
+BASENAME="$IND"_to_"$REF"_w"$WINDOW"
+LOCAL_DIR=/scratch/local/monthly/kjaron/"$SP"_"$BAM"_atlas_theta_w"$WINDOW"
 
 if [[ ! -s $TROOT/data/$SP/mapping/$BAM ]]
 then
@@ -17,30 +23,34 @@ fi
 
 bsub <<< """
 #BSUB -L /bin/bash
-#BSUB -J theta_$1
+#BSUB -J theta_$SP
 #BSUB -q bgee
-#BSUB -o job_"$3"_to_"$2"_w"$WINDOW"_theta.log
-#BSUB -e job_"$3"_to_"$2"_w"$WINDOW"_theta.err
+#BSUB -o job_"$BASENAME"_theta.log
+#BSUB -e job_"$BASENAME"_theta.err
 #BSUB -n 1
 #BSUB -M 20000000
 
 # make local directory for computations
-LOCAL_DIR=/scratch/local/monthly/kjaron/"$SP"_"$BAM"_atlas_theta
-mkdir -p \$LOCAL_DIR
+mkdir -p $LOCAL_DIR
 
 # copy genome and mapped long insert size lib
-cd \$LOCAL_DIR
+cd $LOCAL_DIR
 ln -s $TROOT/data/$SP/mapping/$BAM* .
 
-python3 $TROOT/N_variant_calling/create_BED_withmin_window_size.py \
-	$SP $3 $WINDOW > relevant_windows.bed
-atlas task=estimateTheta bam=$BAM suppressWarnings window=relevant_windows.bed \
-	1> "$3"_to_"$2"_w"$WINDOW"_theta.log 2> "$3"_to_"$2"_w"$WINDOW"_theta.err
+CHLIMIT=$(python3 $TROOT/N_variant_calling/create_BED_withmin_window_size.py $SP $REF $WINDOW)
+atlas task=estimateTheta \
+	bam=$BAM \
+	window=$WINDOW \
+	limitChr=\$CHLIMIT \
+	suppressWarnings verbose \
+	1> "$BASENAME"_theta.log
+
+# lines : window=$WINDOW, limitChr=\$CHLIMIT; have to be adjusted to limit computation only on meaningfull scaffolds
 
 mkdir -p $TROOT/data/$SP/variant_calls/$3/atlas
-mv $(basename $BAM .bam)_theta_estimates.txt \
-	$TROOT/data/$SP/variant_calls/$3/atlas/$(basename $BAM .bam)_w"$WINDOW"_theta_estimates.txt
-mv "$3"_to_"$2"_w"$WINDOW"_theta.* $TROOT/data/$SP/variant_calls/$3/atlas/
+mv "$BAMBASE"_theta_estimates.txt \
+	$TROOT/data/$SP/variant_calls/$IND/atlas/"$BASENAME"_theta_estimates.txt
+mv "$BASENAME"_theta.log $TROOT/data/$SP/variant_calls/$IND/atlas/
 
 rm $BAM*
 rmdir \$LOCAL_DIR
