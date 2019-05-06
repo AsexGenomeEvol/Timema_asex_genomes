72bfa38849177576708f683b2875b0875297d7c1
Kamil S Jaroň <376090@mail.muni.cz>
2019-05-06 11:45:14 +0200

    [variant_analysis] smoove should work now, reducting memory requested for delly, to be tested

--- side_proj/E_structural_variation_calling/smoove.sh
+++ side_proj/E_structural_variation_calling/smoove.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-#activate virtual environment and load all the packages before you call smoove
+# E_structural_variation_calling/smoove.sh
+#   data/1_Tdi/reference/1_Tdi_b3v08.fasta.gz
+#   data/1_Tdi/reference/1_Tdi_b3v08.fasta.gz.fai
+#   data/1_Tdi/reference/1_Tdi_b3v08.fasta.gz.gzi
+#   data/1_Tdi/mapping/Tdi_01_to_b3v08_mapped_within_scfs.bam
+#   data/1_Tdi/mapping/Tdi_01_to_b3v08_mapped_within_scfs.bam.bai
+#   data/1_Tdi/variant_calls/Tdi_01/Tdi_01_smoove
+SAMPLE=$1
+FASTA=$2
+BAM=$5
+OUTDIR=$7
 
-# cluster_script + "smoove call -x --genotype --name {wildcards.sample} -f data/{wildcards.sp}/reference/{wildcards.sp}_b3v08.fasta.gz --processes 32 data/{wildcards.sp}/mapping/{wildcards.sample}_to_b3v08_mapped_within_scfs.bam --outdir {output}"
+source /scratch/beegfs/monthly/kjaron/src/svtyper/bin/activate
+module add UHTS/Analysis/samtools/1.8
 
-
+smoove call -x --genotype --name $SAMPLE -f $FASTA --processes 32 $BAM --outdir $OUTDIR
