b2fb54021ea5c030205a760ebec77f37d5dbd664
Kamil S Jaroň <376090@mail.muni.cz>
2020-08-12 14:16:00 +0200

    [variant_analysis] adjusting sorting variants to add chromosomal information to variants

--- side_proj/D_variant_analysis/plot_SNPs_on_chromosomes.R
+++ side_proj/D_variant_analysis/plot_SNPs_on_chromosomes.R
@@ -1,30 +1,74 @@
-# library(AsexStats)
+library(AsexStats)
 library(RColorBrewer)
+
+# ######
+# # args <- commandArgs(trailingOnly=TRUE)
+# # sp_i <- 7
 
 # asexuals <- seq(1, 10, by=2)
 # to_analyse <- timemas$codes[asexuals]
 sp = '5_Tge'
 
+### LOADING SNPs
 tab_filename <- paste0('data/SNP_calls/', sp, '_reduced_filtered_variants.tsv')
 variant_tab <- read.table(tab_filename, stringsAsFactors = F)
 colnames(variant_tab) <- c('scf', 'pos', 'qual', paste0('g', 1:5), paste0('d', 1:5))
-
-# load genome mapping to cristinae reference
-aln_file <- paste0('data/b3v08_anchoring_to_LGs/', sp, '_scf_block_alignment.tsv')
-genome_aligment <- read.table(aln_file, header = T, stringsAsFactors = F)
+# > head(variant_tab, 2)
+#                      scf    pos    qual  g1  g2  g3  g4  g5 d1 d2 d3 d4 d5
+# 1 5_Tge_b3v08_scaf000001  54809  742.15 1/1 0/0 0/0 0/0 0/0 21 19 23 20 19
+# 2 5_Tge_b3v08_scaf000001 225324 3146.15 1/1 0/0 1/1 1/1 1/1 20 16 32 16 20
 
 ### LOADING INFO ABOUT GENOMES
 scf_len_file <- paste0("data/", sp, "/reference/", sp, "_b3v08_scf.lengths")
 scf_lengths <- read.table(scf_len_file, header = F, row.names = 1, col.names = c('scf', 'len'))
 # scf_to_analyze <- lapply(scf_lengths, function(x){rownames(x)[x[,1] > 100000]})
+# > head(scf_lengths)
+#                            len
+# 5_Tge_b3v08_scaf000001 1405450
+# 5_Tge_b3v08_scaf000002 1258996
+# source('../timema_assembly/scripts/prepare_reference.R')
 
-### LOADING SNPs
-# data/<sp>/reference/<sp>_b3v08_scf.lengths
-# data/b3v08_anchoring_to_LGs/<sp>_scf_block_alignment.tsv
-# data/SNP_calls/<sp>_reduced_filtered_variants.tsv
+reference <- read.table('data/external_ref/sex_lg_assigment_scores_1.4a.tsv')
+colnames(reference) <- c('scf_o', 'scf', 'score', 'cov', 'len', 'asignment')
+reference$chromosome <- sapply(strsplit(reference$scf, "_"), function(x) { x[1] } )
 
-# ######
-# # args <- commandArgs(trailingOnly=TRUE)
-# # sp_i <- 7
-source('../timema_assembly/scripts/prepare_reference.R')
+chromosomes <- data.frame(chr = paste0('lg', c(1:12, 'X')))
+
+get_chr_size <- function(chr){
+    # when scaffolds are mapped to reference, I always leave 10000 bases between them on a linkage group (consistent with the NCBI reference that used this arbitrary number of bases)
+    sum(reference[reference$chromosome == chr,'len']) + ((sum(reference$chromosome == chr) - 1) * 10000)
+}
+
+chromosomes$len <- sapply( chromosomes$chr, get_chr_size )
+
+###############################
+##### plotting parameters #####
+###############################
+
+window = 1e6
+gap_beween_chromosomes = 3
+
+###############################
+
+# I need to round up chromosome lengths to avoid one window being part of two chromosomes
+chromosomes$rounded_len <- ceiling(chromosomes$len / window) * window + (window * gap_beween_chromosomes)
+chromosomes$adjustments <- cumsum(c(0, chromosomes$rounded_len[1:(nrow(chromosomes) - 1)]))
+rownames(chromosomes) <- chromosomes$chr
+chromosomes <- chromosomes[chromosomes$chr != 'lgX', ]
+
+get_lg_windows <- function(i) {
+    all_windows <- seq(0, chromosomes[i,'rounded_len'], by = window)
+    adj <- chromosomes[i, 'adjustments']
+    lg_from <- all_windows[1:(length(all_windows) - 1)]
+    lg_to <- all_windows[2:length(all_windows)]
+    data.frame(chr = chromosomes[i, 'chr'],
+               lg_from = lg_from,
+               lg_to = lg_to,
+               genome_from = lg_from + adj,
+               genome_to = lg_from + adj)
+}
+
+variant_density_table <- do.call("rbind", lapply(1:12, get_lg_windows))
+variant_density_table$variants <- 0
+
 
--- side_proj/D_variant_analysis/sorting_variants.py
+++ side_proj/D_variant_analysis/sorting_variants.py
@@ -8,19 +8,106 @@ def test_depth(depth):
         return int(depth) > 15
 
 from collections import defaultdict
+from interval import interval
+from sys import stderr
+
+class reference_block:
+    def __init__(self, line_tab):
+        self.query_from = int(line_tab[3])
+        self.query_to = int(line_tab[4])
+        self.scf = line_tab[5]
+        self.scf_from = int(line_tab[6])
+        self.scf_to = int(line_tab[7])
+        self.lg = line_tab[8]
+        self.lg_from = int(line_tab[9])
+        self.lg_to = int(line_tab[10])
+    def lg_pos(self, pos):
+        aln_size = self.query_to - self.query_from
+        ref_aln_size = self.lg_to - self.lg_from
+        rel_distance_from_left = (pos - self.query_from) / aln_size
+        if self.lg_to > self.lg_from:
+            pos = self.lg_from + round(rel_distance_from_left * ref_aln_size)
+        else:
+            pos = self.lg_to + round((1 - rel_distance_from_left) * ref_aln_size)
+        return(pos)
+    def scf_pos(self, pos):
+        aln_size = self.query_to - self.query_from
+        ref_aln_size = self.scf_to - self.scf_from
+        rel_distance_from_left = (pos - self.query_from) / aln_size
+        if self.scf_to > self.scf_from:
+            pos = self.scf_from + round(rel_distance_from_left * ref_aln_size)
+        else:
+            pos = self.scf_to + round((1 - rel_distance_from_left) * ref_aln_size)
+        return(pos)
+
+
 
 for sp in ['1_Tdi', '1_Tps', '2_Tcm', '2_Tsi', '3_Tce', '3_Tms', '4_Tbi', '4_Tte', '5_Tge', '5_Tpa']:
+    stderr.write("processing {}\n".format(sp))
+
     variant_file = 'data/SNP_calls/' + sp + '.SNP_filter_passed.tsv'
+    mapping_file = 'data/b3v08_anchoring_to_LGs/' + sp + '_scf_block_alignment.tsv'
     reduced_variants_file = 'data/SNP_calls/' + sp + '_reduced_variants.tsv'
     filtered_variants_file = 'data/SNP_calls/' + sp + '_reduced_filtered_variants.tsv'
-    with open(variant_file) as f, open(reduced_variants_file, 'w') as reduced_variants, open(filtered_variants_file, 'w') as filtered_variants:
-        for line in f:
+
+    # add here also the LG / pos information
+    scf2intervals = defaultdict(list)
+    scf2mapping = defaultdict(list)
+    with open(mapping_file) as mf:
+        header = mf.readline()
+        for line in mf:
+            mapped_block_tab = line.rstrip('\n').split('\t')
+            mapped_interval = interval([int(mapped_block_tab[3]), int(mapped_block_tab[4])])
+            scf2intervals[mapped_block_tab[0]].append(mapped_interval)
+            scf2mapping[mapped_block_tab[0]].append(reference_block(mapped_block_tab))
+
+    stderr.write("\tmapping loaded\n")
+
+
+    with open(variant_file) as vf, open(reduced_variants_file, 'w') as reduced_variants, open(filtered_variants_file, 'w') as filtered_variants:
+        unmapped = 0
+        filtered = 0
+        total = 0
+        for line in vf:
+            total += 1
             variant = line.split()
             genotypes = [v.split(':')[0] for v in variant[3:]]
             depths = [v.split(':')[2] for v in variant[3:]]
             reference = variant[0][:10] + '8' + variant[0][11:]
-            pos = variant[1]
+            pos = int(variant[1])
             qual = variant[2]
-            reduced_variants.write(reference + '\t' + pos + '\t' + qual + '\t' + '\t'.join(genotypes) + '\t' + '\t'.join(depths) + '\n')
+            reduced_variants.write(reference + '\t' + str(pos) + '\t' + qual + '\t' + '\t'.join(genotypes) + '\t' + '\t'.join(depths) + '\n')
             if float(qual) > 300 and any([test_depth(d) for d in depths]):
-                filtered_variants.write(reference + '\t' + pos + '\t' + qual + '\t' + '\t'.join(genotypes) + '\t' + '\t'.join(depths) + '\n')
+                mapped_to = list()
+                for i, genomic_interval in enumerate(scf2intervals[reference]):
+                    if pos in genomic_interval:
+                        mapped_to.append(i)
+                if len(mapped_to) == 0:
+                    # not mapped
+                    last_four_cols = "\tNA\tNA\tNA\tNA\n" # these two look the same
+                    unmapped += 1
+
+                if len(mapped_to) > 1:
+                    # multimapping
+                    lg = scf2mapping[reference][mapped_to[0]].lg
+                    scf = scf2mapping[reference][mapped_to[0]].scf
+                    if not all([scf2mapping[reference][i].lg == lg for i in mapped_to]):
+                        lg = "NA"
+                    if not all([scf2mapping[reference][i].scf == scf for i in mapped_to]):
+                        scf = "NA"
+                    last_four_cols = "\t{}\tNA\t{}\tNA\n".format(scf, lg)
+
+                if len(mapped_to) == 1:
+                    # unique mapping
+                    mapping = scf2mapping[reference][mapped_to[0]]
+                    last_four_cols = "\t{}\t{}\t{}\t{}\n".format(mapping.scf, mapping.scf_pos(pos), mapping.lg, mapping.lg_pos(pos))
+
+                filtered_variants.write(reference + '\t' + str(pos) + '\t' + qual + '\t' + '\t'.join(genotypes) + '\t' + '\t'.join(depths) + last_four_cols)
+
+            else:
+                filtered += 1
+
+        stderr.write("\tprocessed total {} variants\n".format(total))
+        stderr.write("\tfiltered {} variants\n".format(filtered))
+        stderr.write("\tunmapped {} variants (not counting multimapping)\n".format(unmapped))
+        stderr.write("\tdone\n")
