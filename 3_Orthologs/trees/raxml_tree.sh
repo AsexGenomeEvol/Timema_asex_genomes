afe5487b2bddbbc74291407a8c01104b900f230d
DarrenJParker <darrenjamesparker@gmail.com>
2022-05-31 17:14:30 +0100

    tree update

--- 3_Orthologs/trees/raxml_tree.sh
+++ 3_Orthologs/trees/raxml_tree.sh
@@ -9,30 +9,31 @@
 
 
 ############################################################################################
-### Final Alignment is here 3_Orthologs/trees/tree_data/align_five_pair_HOGs_genomeonly_gb.fa 
-### get aligmnents of orthologs from selectome (1-to-1, five pair, genome copies only, codons)
+### Final Alignment is here 3_Orthologs/trees/tree_data/align_five_pair_HOGs_all.fa 
+### get aligmnents of orthologs from selectome (1-to-1, five pair, codons)
 ## join alignments
 ## filter alignment with gblocks
 
-~/Gblocks_0.91b/Gblocks align_five_pair_HOGs_genomeonly.fa -t=c -e=-gb1 -b4=12 
-#Original alignment: 6272457 positions
-#Gblocks alignment:  1523166 positions (24 %) in 4520 selected block(s)
+~/Gblocks_0.91b/Gblocks align_five_pair_HOGs_all.fa -t=c -e=-gb1 -b4=12 
+#Original alignment: 10503174 positions
+#Gblocks alignment:  2377398 positions (22 %) in 7788 selected block(s)
+
 
 ############################################################################################
 ## draw tree
 module add Bioinformatics/Software/vital-it
 module load Phylogeny/raxml/8.2.12
 
-cat > codon_specify_genome_only.txt ## 3_Orthologs/trees/tree_data/codon_specify_genome_only.txt
-DNA, codon1 = 1-1523166\3
-DNA, codon2 = 2-1523166\3
-DNA, codon3 = 3-1523166\3
+cat > codon_specify_all.txt ## 3_Orthologs/trees/tree_data/codon_specify_all.txt
+DNA, codon1 = 1-2377398\3
+DNA, codon2 = 2-2377398\3
+DNA, codon3 = 3-2377398\3
 
-raxmlHPC -m GTRGAMMA -n five_pair_HOGs_genomeonly_1000boot -s align_five_pair_HOGs_genomeonly_gb.fa -p 12345  -# 1000 -q codon_specify_genome_only.txt -f a -x 12345 -c 40 -T 1
+raxmlHPC -m GTRGAMMA -n five_pair_HOGs_all_1000boot -s align_five_pair_HOGs_all_gb.fa -p 12345  -# 1000 -q codon_specify_all.txt -f a -x 12345 -c 40 -T 1
 
 
 ### Tree here:
-# 3_Orthologs/trees/output/RAxML_bipartitions.five_pair_HOGs_genomeonly_1000boot
+# 3_Orthologs/trees/output/RAxML_bipartitions.five_pair_HOGs_all_1000boot
 
 
 
